7c8655b530dd30403a793b7e74b18c9a836c969b
negrilaim <negrilaim@gmail.com>
2023-03-08 12:22:36 +0300

    Added some logic to Header  and finish it

--- src/Components/Head/Header.jsx
+++ src/Components/Head/Header.jsx
@@ -1,43 +1,52 @@
-import React from 'react'
-import "../Head/Header.css"
+import React, { useState } from "react"
+import "./Header.css"
+
 
 const Header = () => {
+  // fixed Header
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header")
+    header.classList.toggle("active", window.scrollY > 100)
+  })
+  // Toogle Menu
+  const [Mobile, setMobile] = useState(false)
   return (
     <>
-        <header className='header'>
-            <div className="container d_flex">
-                <div className='logo'>
-                    <img src="" alt="" />
-                </div>
-                <div className="navlink">
-                    <ul className='link f_flex uppercase'>
-                        <li>
-                            <a href="/home"> Home</a>
-                        </li>
-                        <li>
-                            <a href="/features">features</a>
-                        </li>
-                        <li>
-                            <a href="/portofolio">portofolio</a>
-                        </li>
-                        <li>
-                            <a href="/resume">resume</a>
-                        </li>
-                        <li>
-                            <a href="/clients">clients</a>
-                        </li>
-                        <li>
-                            <a href="/blog">blog</a>
-                        </li>
-                        <li>
-                            <a href="/contact">contact</a>
-                        </li>
-                        <li><button className='home-btn'>Buy Now</button></li>
-                    </ul>
-                    <button className='toggle'><i className='fas fa-times'></i></button>
-                </div>
-            </div>
-        </header>
+      <header className='header'>
+        <div className='container d_flex'>
+          <div className='logo'>
+            <p>LOGO</p>
+          </div>
+
+          <div className='navlink'>
+            <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
+              {/*<ul className='link f_flex uppercase {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}'>*/}
+              <li>
+                <a href='#home'>home</a>
+              </li>
+              <li>
+                <a href='#features'>features</a>
+              </li>
+              <li>
+                <a href='#portfolio'>portfolio</a>
+              </li>
+              <li>
+                <a href='#resume'>resume</a>
+              </li>
+              <li>
+                <a href='#contact'>contact</a>
+              </li>
+              <li>
+                <button className='home-btn'>BUY NOW</button>
+              </li>
+            </ul>
+
+            <button className='toggle' onClick={() => setMobile(!Mobile)}>
+              {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
+            </button>
+          </div>
+        </div>
+      </header>
     </>
   )
 }
