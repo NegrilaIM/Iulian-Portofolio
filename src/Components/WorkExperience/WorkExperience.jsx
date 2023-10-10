962f55ebc9cac7e8537a2e68a6bf5c583c5d31d3
negrilaim <negrilaim@gmail.com>
2023-10-10 13:31:17 +0300

    completed some things

--- src/Components/WorkExperience/WorkExperience.jsx
+++ src/Components/WorkExperience/WorkExperience.jsx
@@ -4,49 +4,51 @@ import Card from './Card'
 import './WorkExperience.css'
 
 const WorkExperience = () => {
-  return (
-    <>
-        <section className='Resume' id='resume'>
-            <div className="container top">
-                <div className="heading text-center">
-                    <h1>Education and Work Experience</h1>
-                </div>
-                <div className="content-section mtop d_flex">
-                    <div className="left">
-                        <div className="heading">
-                            <h4 >2012-2022</h4>
-                            <h1 >Education</h1>
+    return (
+        <>
+            <section className='Resume' id='resume'>
+                <div className="container top">
+                    <div className="heading text-center">
+                        <h1>Education and Work Experience</h1>
+                    </div>
+                    <div className="content-section mtop d_flex">
+                        <div className="left">
+                            <div className="heading">
+                                <h4 >2012-2022</h4>
+                                <h1 >Education</h1>
+                            </div>
+                            <div className="content">
+                                {Achievements.map((ach, index) => {
+                                    if (ach.category === 'education') {
+                                        return (
+                                            <Card key={index} title={ach.title} year={ach.year} rate= {ach.rate} description={ach.description} prof={ach.prof}/>
+                                        )
+                                    }
+                                    return null; 
+                                })}
+                            </div>
                         </div>
-                        <div className="content">
-                            {Achievements.map((ach, index) => {
-                                if (ach.category === 'education') {
-                                    return (
-                                        <Card key={index} title={ach.title} year={ach.year} rate= {ach.rate} description={ach.description} prof={ach.prof}/>
-                                    )
-                                }
-                            })}
+                        <div className="left">
+                            <div className="heading">
+                                <h4>2016-2023</h4>
+                                <h1>Work Experience</h1>
+                            </div>
+                            <div className="content">
+                                {Achievements.map((ach, index) => {
+                                    if (ach.category === 'experience') {
+                                        return (
+                                            <Card key={index} title={ach.title} year={ach.year} rate= {ach.rate} description={ach.description} prof={ach.prof}/>
+                                        )
+                                    }
+                                    return null;
+                                })}
+                            </div>
                         </div>
                     </div>
-                    <div className="left">
-                        <div className="heading">
-                            <h4>2016-2023</h4>
-                            <h1>Work Experience</h1>
-                        </div>
-                        <div className="content">
-                            {Achievements.map((ach, index) => {
-                                if (ach.category === 'experience') {
-                                    return (
-                                        <Card key={index} title={ach.title} year={ach.year} rate= {ach.rate} description={ach.description} prof={ach.prof}/>
-                                    )
-                                }
-                            })}
-                        </div>
-                    </div>
                 </div>
-            </div>
-        </section>
-    </>
-  )
+            </section>
+        </>
+      )
 }
 
 export default WorkExperience
